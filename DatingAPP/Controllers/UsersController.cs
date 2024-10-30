fbf5277611d5183534b6d6c59a4167c6e4b4e10f
Mohamed Tharwat <114985142+Mohamedtharwat8@users.noreply.github.com>
2024-10-30 21:14:35 +0300

    Add project files.

---
+++ DatingAPP/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers() {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var users = _context.Users.Find(id);
+            if (User == null)
+                return NotFound();
+            return Ok(users);
+        }
+
+
+    }
+}
